d8d464768d824ec3f24b6b1dd22b0c84bc165e77
adriferracuti <1831360+adriferracuti@users.noreply.github.com>
2022-06-01 09:18:00 +0200

    k6 load testing script

---
+++ k6/script.js
@@ -0,0 +1,23 @@
+import http from 'k6/http';
+import { check } from 'k6';
+import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
+
+export const options = {
+  vus: 1000,
+  duration: '10s',
+  gracefulStop: '0s'
+};
+
+export default function () {
+  let resp = http.get('http://localhost:8080/webclient?delay=1000');
+  check(resp, {
+    "is status 200": (r) => r.status === 200
+  })
+  // sleep(1);
+}
+
+export function handleSummary(data) {
+  return {
+    "summary.html": htmlReport(data),
+  };
+}

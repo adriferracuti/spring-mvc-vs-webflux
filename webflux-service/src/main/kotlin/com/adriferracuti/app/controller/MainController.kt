202cef89713612402179ef25b9d2d7c193cabc7a
adriferracuti <1831360+adriferracuti@users.noreply.github.com>
2022-10-18 18:49:06 +0200

    renamed apps, increased web client connections

--- webflux-service/src/main/kotlin/com/adriferracuti/app/controller/MainController.kt
+++ webflux-service/src/main/kotlin/com/adriferracuti/app/controller/MainController.kt
@@ -2,21 +2,38 @@ package com.adriferracuti.app.controller
 
 import com.adriferracuti.app.entity.Dog
 import mu.KotlinLogging
+import org.springframework.http.client.reactive.ReactorClientHttpConnector
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.reactive.function.client.WebClient
 import reactor.core.publisher.Mono
+import reactor.netty.http.client.HttpClient.*
+import reactor.netty.resources.ConnectionProvider
 import java.net.URI
 import java.net.http.HttpClient
 import java.net.http.HttpRequest
 import java.net.http.HttpResponse
+import java.time.Duration.ofSeconds
 import java.util.concurrent.Executors
 import java.util.concurrent.atomic.AtomicInteger
 
 @RestController
 class MainController {
-    private val webClient: WebClient = WebClient.builder().baseUrl("http://localhost:8080").build()
+
+    var provider: ConnectionProvider = ConnectionProvider.builder("custom")
+        .maxConnections(2000)
+        .maxIdleTime(ofSeconds(20))
+        .maxLifeTime(ofSeconds(60))
+        .pendingAcquireTimeout(ofSeconds(60))
+        .evictInBackground(ofSeconds(120))
+        .build()
+
+    private val webClient: WebClient = WebClient.builder()
+        .baseUrl("http://localhost:8080")
+        .clientConnector(ReactorClientHttpConnector(create(provider)))
+        .build()
+
     private val httpClient: HttpClient = HttpClient.newBuilder()
         .executor(Executors.newSingleThreadExecutor()).build()
 
